9885abe7cd95690daef28cce19960336c0c7774b
Christina Herrick <c.herrick@unh.edu>
2024-01-31 10:46:32 -0500

    fixed reversed x,y; added overwrite
    Fixed so script identifies lat/lon correctly in original files;
    
    reorganized to add logic for skipping sites or checking for overwrite

--- make_shps.py
+++ make_shps.py
@@ -16,27 +16,34 @@ outspace = os.path.join(os.getcwd(), "out")
 env.workspace = outspace
 env.overwriteOutput = True
 
-skipLakes = ['Agrio']
+skipLakes = []
+overwrite = False
 
-# if not os.path.exists(os.path.join(outspace, "Hysplit_sites.gdb")):
-#     arcpy.management.CreateFileGDB(outspace, "Hysplit_sites")
-# gdb_out = os.path.join(outspace, "Hysplit_sites.gdb")
+def writeOverwrite(input_path):
+    if os.path.exists(input_path):
+        return overwrite
+    else:
+        return True
+
+if not os.path.exists(os.path.join(outspace, "Hysplit_sites.gdb")):
+    arcpy.management.CreateFileGDB(outspace, "Hysplit_sites")
 
 for d in os.listdir(inspace):
     if os.path.isdir(os.path.join(inspace, d)) and d not in skipLakes:
+
         site = d
         site_dir = os.path.join(inspace, d)
 
         print('site: %s --> %s' % (site, strftime('%X')))
 
-        all = pd.DataFrame(columns=['year','month', 'day','hour','x','y','z','P','id'])
-        all_wet = pd.DataFrame(columns=['year','month', 'day','hour','x','y','z','P','id'])
-        all_dry = pd.DataFrame(columns=['year','month', 'day','hour','x','y','z','P','id'])
-        all_drought = pd.DataFrame(columns=['year','month', 'day','hour','x','y','z','P','id'])
-        all_non_drought = pd.DataFrame(columns=['year','month', 'day','hour','x','y','z','P','id'])
+        all = pd.DataFrame(columns=['year','month', 'day','hour','y','x','z','P','id'])
+        all_wet = pd.DataFrame(columns=['year','month', 'day','hour','y','x','z','P','id'])
+        all_dry = pd.DataFrame(columns=['year','month', 'day','hour','y','x','z','P','id'])
+        all_drought = pd.DataFrame(columns=['year','month', 'day','hour','y','x','z','P','id'])
+        all_non_drought = pd.DataFrame(columns=['year','month', 'day','hour','y','x','z','P','id'])
 
         for f in os.listdir(site_dir):
-            colnames = ['id1', 'id2', 'year','month', 'day','hour','min','sec','diff','x','y','z','P']
+            colnames = ['id1', 'id2', 'year','month', 'day','hour','min','sec','diff','y','x','z','P']
             
             f_csv = os.path.join(site_dir, f+'.csv')
             f_id = re.sub('\D', '', f)
@@ -63,30 +70,55 @@ for d in os.listdir(inspace):
             else:
                 all_non_drought = pd.concat([all_non_drought, df])
 
-        print("Writing geometries... (%s)" % (strftime('%X')))
-        all['geom'] = all.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
-        all_wet['geom'] = all_wet.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
-        all_dry['geom'] = all_dry.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
-        all_drought['geom'] = all_drought.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
-        all_non_drought['geom'] = all_non_drought.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+        files_to_gdb = []
 
-        print("Writing geopandas df... (%s)" % strftime('%X'))
-        shp_all = gpd.GeoDataFrame(all, geometry='geom', crs='EPSG:4326')
-        shp_wet = gpd.GeoDataFrame(all_wet, geometry='geom', crs='EPSG:4326')
-        shp_dry = gpd.GeoDataFrame(all_dry, geometry='geom', crs='EPSG:4326')
-        shp_drought = gpd.GeoDataFrame(all_drought, geometry='geom', crs='EPSG:4326')
-        shp_non_drought = gpd.GeoDataFrame(all_non_drought, geometry='geom', crs='EPSG:4326')
+        if writeOverwrite(os.path.join(outspace, site+"_all.shp")):
+            print("Writing 'all' geometries... (%s)" % (strftime('%X')))
+            all['geom'] = all.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+            print("Writing 'all' geopandas df... (%s)" % strftime('%X'))
+            shp_all = gpd.GeoDataFrame(all, geometry='geom', crs='EPSG:4326')
+            print("Writing 'all' to local files...  (%s)" % strftime('%X'))
+            shp_all.to_file(os.path.join(outspace, site+"_all.shp"), driver='ESRI Shapefile', index=False)
+            files_to_gdb.append(site+"_all.shp")
 
-        print("Writing to local files...  (%s)" % strftime('%X'))
-        shp_all.to_file(os.path.join(outspace, site+"_all.shp"), driver='ESRI Shapefile', index=False)
-        shp_wet.to_file(os.path.join(outspace, site+"_wet.shp"), driver='ESRI Shapefile', index=False)
-        shp_dry.to_file(os.path.join(outspace, site+"_dry.shp"), driver='ESRI Shapefile', index=False)
-        shp_drought.to_file(os.path.join(outspace, site+"_drought.shp"), driver='ESRI Shapefile', index=False)
-        shp_non_drought.to_file(os.path.join(outspace, site+"_nondrought.shp"), driver='ESRI Shapefile', index=False)
+        if writeOverwrite(os.path.join(outspace, site+"_wet.shp")):
+            print("Writing 'wet' geometries... (%s)" % (strftime('%X')))
+            all_wet['geom'] = all_wet.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+            print("Writing 'wet' geopandas df... (%s)" % strftime('%X'))
+            shp_wet = gpd.GeoDataFrame(all_wet, geometry='geom', crs='EPSG:4326')
+            print("Writing 'wet' to local files...  (%s)" % strftime('%X'))
+            shp_wet.to_file(os.path.join(outspace, site+"_wet.shp"), driver='ESRI Shapefile', index=False)
+            files_to_gdb.append(site+"_wet.shp")
+
+        if writeOverwrite(os.path.join(outspace, site+"_dry.shp")):
+            print("Writing 'dry' geometries... (%s)" % (strftime('%X')))
+            all_dry['geom'] = all_dry.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+            print("Writing 'dry' geopandas df... (%s)" % strftime('%X'))
+            shp_dry = gpd.GeoDataFrame(all_dry, geometry='geom', crs='EPSG:4326')
+            print("Writing 'dry' to local files...  (%s)" % strftime('%X'))
+            shp_dry.to_file(os.path.join(outspace, site+"_dry.shp"), driver='ESRI Shapefile', index=False)
+            files_to_gdb.append(site+"_dry.shp")
+
+        if writeOverwrite(os.path.join(outspace, site+"_drought.shp")):
+            print("Writing 'drought' geometries... (%s)" % (strftime('%X')))
+            all_drought['geom'] = all_drought.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+            print("Writing 'drought' geopandas df... (%s)" % strftime('%X'))
+            shp_drought = gpd.GeoDataFrame(all_drought, geometry='geom', crs='EPSG:4326')
+            print("Writing 'drought' to local files...  (%s)" % strftime('%X'))
+            shp_drought.to_file(os.path.join(outspace, site+"_drought.shp"), driver='ESRI Shapefile', index=False)
+            files_to_gdb.append(site+"_drought.shp")
+
+        if writeOverwrite(os.path.join(outspace, site+"_nondrought.shp")):
+            print("Writing 'nondrought' geometries... (%s)" % (strftime('%X')))
+            all_non_drought['geom'] = all_non_drought.apply(lambda rec: Point((float(rec.x), float(rec.y))), axis=1)
+            print("Writing 'nondrought' geopandas df... (%s)" % strftime('%X'))
+            shp_non_drought = gpd.GeoDataFrame(all_non_drought, geometry='geom', crs='EPSG:4326')
+            print("Writing 'nondrought' to local files...  (%s)" % strftime('%X'))
+            shp_non_drought.to_file(os.path.join(outspace, site+"_nondrought.shp"), driver='ESRI Shapefile', index=False)
+            files_to_gdb.append(site+"_nondrought.shp")
 
         print("Writing to gdb...  (%s)" % strftime('%X'))
         arcpy.conversion.FeatureClassToGeodatabase([site+"_all.shp", site+"_wet.shp", site+"_dry.shp", site+"_drought.shp", site+"_nondrought.shp"], 
-                                                   'Hysplit_sites.gdb')
+                                                'Hysplit_sites.gdb')
 
         print("Done!\n")
-            
